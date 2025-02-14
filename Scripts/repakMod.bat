2f2b4c68079057bccaeebba768303319ea49324a
natimerry <soham.nandy2006@gmail.com>
2025-02-14 11:46:47 +0530

    Auto append _9999999_P to the end of the patch packed mod. THanks to `potsuuu` on discord

--- Scripts/repakMod.bat
+++ Scripts/repakMod.bat
@@ -20,30 +20,85 @@ for %%F in (%*) do (
     if exist "%%F" (
         if exist "%%F\*" (
             echo ======================================
-            echo ======================================
             echo Processing folder: %%F
             @pushd %~dp0
+            :: Generate the .pak file from folder
             .\repak.exe --aes-key 0C263D8C22DCB085894899C3A3796383E9BF9DE0CBFB08C9BF2DEF2E84F29D74 pack %%F --version V11 --verbose --patch-uasset --compression Oodle
+
+            :: Get folder name
+            set "folderName=%%~nF"
+            set "has9999999=0"
+            set "hasP=0"
+
+            :: Check if _9999999 is already present
+            echo !folderName! | findstr /i "_9999999" >nul
+            if !errorlevel! == 0 set "has9999999=1"
+
+            :: Check if _P is already present
+            echo !folderName! | findstr /i "_P" >nul
+            if !errorlevel! == 0 set "hasP=1"
+
+            :: Construct new name
+            set "newName=!folderName!"
+            if !has9999999! == 0 set "newName=!newName!_9999999"
+            if !hasP! == 0 set "newName=!newName!_P"
+
+            :: Ensure order (_9999999 before _P)
+            set "newName=!newName:_P_9999999=_9999999_P!"
+
+            set "newName=!newName!.pak"
+            echo Renaming .pak to !newName!
+            ren "%%~dpF\%%~nF.pak" "!newName!"
+
             @popd
         ) else (
-			set "fileExt=%%~xF"
+            set "fileExt=%%~xF"
             :: convert to lowercase
             for %%A in (!fileExt!) do set "fileExt=%%A"
-			
+
             if /i "!fileExt!"==".pak" (
-				echo Processing .pak file: %%F
-				
-				if exist "%%~dpnF\*" ( set /a keepDir = 1 )
-				
-				@pushd %%~dpF
+                echo Processing .pak file: %%F
+
+                if exist "%%~dpnF\*" ( set /a keepDir = 1 )
+
+                @pushd %%~dpF
+                :: Unpack the .pak file
                 .\repak.exe --aes-key 0C263D8C22DCB085894899C3A3796383E9BF9DE0CBFB08C9BF2DEF2E84F29D74 unpack %%F --verbose
-				@del %%F
+                @del %%F
+
+                :: Pack the folder back with the new name
                 .\repak.exe --aes-key 0C263D8C22DCB085894899C3A3796383E9BF9DE0CBFB08C9BF2DEF2E84F29D74 pack "%%~dpnF" --version V11 --verbose --patch-uasset --compression Oodle
+
+                :: Get folder name
+                set "folderName=%%~nF"
+                set "has9999999=0"
+                set "hasP=0"
+
+                :: Check if _9999999 is already present
+                echo !folderName! | findstr /i "_9999999" >nul
+                if !errorlevel! == 0 set "has9999999=1"
+
+                :: Check if _P is already present
+                echo !folderName! | findstr /i "_P" >nul
+                if !errorlevel! == 0 set "hasP=1"
+
+                :: Construct new name
+                set "newName=!folderName!"
+                if !has9999999! == 0 set "newName=!newName!_9999999"
+                if !hasP! == 0 set "newName=!newName!_P"
+
+                :: Ensure order (_9999999 before _P)
+                set "newName=!newName:_P_9999999=_9999999_P!"
+
+                set "newName=!newName!.pak"
+                echo Renaming .pak to !newName!
+                ren "%%~dpnF.pak" "!newName!"
+
                 @popd
-				
-				if !keepDir! == 0 ( @RD /S /Q "%%~dpnF" )
-            ) else ( echo File is nor a .pak file or a folder. )
-		)
+
+                if !keepDir! == 0 ( @RD /S /Q "%%~dpnF" )
+            ) else ( echo File is not a .pak file or a folder. )
+        )
     )
 )
 
